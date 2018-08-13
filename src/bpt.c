ade3dde1bbcb1bad4236545d864c6eac3864cec7
Jihun Kim <starlettkim@gmail.com>
2018-08-13 17:30:05 +0900

    함수들이 Page * 를 사용하게 변경. 요거 없으면 나중에 read/write를 못함

--- src/bpt.c
+++ src/bpt.c
@@ -6,6 +6,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define HEADER(ptr)   ((HeaderPage *)((ptr)->ptr_page))
+#define FREE(ptr)     ((FreePage *)((ptr)->ptr_page))
+#define LEAF(ptr)     ((LeafPage *)((ptr)->ptr_page))
+#define INTERNAL(ptr) ((InternalPage *)((ptr)->ptr_page))
 
 // GLOBALS & CONSTANTS.
 
@@ -20,38 +24,38 @@ const int INTERNAL_ORDER = 249;
 // FUNCTION DEFINITIONS.
 
 // Find and return leaf page using binary search.
-LeafPage * find_leaf(bpt_key_t key) {
+Page * find_leaf(bpt_key_t key) {
     if (header_page == NULL) return NULL;
-    InternalPage * page_ptr = (InternalPage *)read_page(header_page->root_page_offset);
+    Page * page_ptr = read_page(header_page->root_page_offset);
 
-    while (!page_ptr->header.is_leaf) {
-        int left = -1, right = page_ptr->header.number_of_keys - 2;
+    while (!INTERNAL(page_ptr)->header.is_leaf) {
+        int left = -1, right = INTERNAL(page_ptr)->header.number_of_keys - 2;
         while (left < right) {
             int mid = (left + right) >> 1;
-            bpt_key_t mid_key = page_ptr->key_offset_pairs[mid + 1].key;
+            bpt_key_t mid_key = INTERNAL(page_ptr)->key_offset_pairs[mid + 1].key;
             if (key < mid_key) right = mid;
             else left = mid + 1;
         }
 
         free(page_ptr);
         u_int64_t next_page_offset = right == -1 ?
-            page_ptr->one_more_page : page_ptr->key_offset_pairs[right].page_offset;
-        page_ptr = (InternalPage *)read_page(next_page_offset);
+            INTERNAL(page_ptr)->one_more_page : INTERNAL(page_ptr)->key_offset_pairs[right].page_offset;
+        page_ptr = read_page(next_page_offset);
     }
 
-    return (LeafPage *)page_ptr;
+    return page_ptr;
 }
 
 /* Find the first record in the 'leaf_page' which is not less than 'key'.
  * If success, return the index of lower bound. Otherwise, return -1.
  * When all records are less than 'key', return the last index.
  */
-int find_lower_bound_at_leaf(LeafPage * leaf_page, bpt_key_t key) {
-    if (!leaf_page) return -1;
-    int left = 0, right = leaf_page->header.number_of_keys - 1;
+int find_lower_bound_at_leaf(Page * leaf_page, bpt_key_t key) {
+    if (!leaf_page || !leaf_page->ptr_page) return -1;
+    int left = 0, right = LEAF(leaf_page)->header.number_of_keys - 1;
     while (left < right) {
         int mid = (left + right) >> 1;
-        bpt_key_t mid_key = leaf_page->records[mid].key;
+        bpt_key_t mid_key = LEAF(leaf_page)->records[mid].key;
         
         if (mid_key >= key) right = mid;
         else left = mid + 1;
@@ -62,21 +66,21 @@ int find_lower_bound_at_leaf(LeafPage * leaf_page, bpt_key_t key) {
 /* Find index of key in leaf_page.
  * If found, return index of the key. Otherwise, return -1.
  */
-int find_at_leaf(LeafPage * leaf_page, bpt_key_t key) {
+int find_at_leaf(Page * leaf_page, bpt_key_t key) {
     int idx = find_lower_bound_at_leaf(leaf_page, key);
-    if (idx < 0 || leaf_page->header.number_of_keys == idx) {
+    if (idx < 0 || LEAF(leaf_page)->header.number_of_keys == idx) {
         return -1;
     }
-    return -(leaf_page->records[idx].key != key);   // Does lower bound contain 'key'?
+    return -(LEAF(leaf_page)->records[idx].key != key);   // Does lower bound contain 'key'?
 }
 
 /* Find the record containing input ‘key’.
  * If found matching ‘key’, return matched ‘value’ string. Otherwise, return NULL.
  */
 c_bpt_value_t find(bpt_key_t key) {
-    LeafPage * leaf_page = find_leaf(key);
+    Page * leaf_page = find_leaf(key);
     int idx = find_at_leaf(leaf_page, key);
-    bpt_value_t val = idx < 0 ? NULL : leaf_page->records[idx].value;
+    bpt_value_t val = idx < 0 ? NULL : LEAF(leaf_page)->records[idx].value;
     free(leaf_page);
     return val;
 }
@@ -84,20 +88,21 @@ c_bpt_value_t find(bpt_key_t key) {
 /* Insert record into leaf page.
  * If success, return 0. Otherwise, return -1.
  */
-int insert_into_leaf(LeafPage * leaf_page, bpt_key_t key, c_bpt_value_t value) {
-    if (leaf_page->header.number_of_keys == LEAF_ORDER) return -1;
+int insert_into_leaf(Page * leaf_page, bpt_key_t key, c_bpt_value_t value) {
+    if (LEAF(leaf_page)->header.number_of_keys == LEAF_ORDER) return -1;
 
     int idx = find_lower_bound_at_leaf(leaf_page, key);
     if (idx < 0) return -1;     // Error
-    if (idx != leaf_page->header.number_of_keys && leaf_page->records[idx].key == key) {
+    if (idx != LEAF(leaf_page)->header.number_of_keys && LEAF(leaf_page)->records[idx].key == key) {
         return -1;  // Key duplication
     }
 
-    int shift_sz = sizeof(leaf_page->records[0]);
-    memcpy(leaf_page->records + idx + shift_sz, leaf_page->records + idx, leaf_page->header.number_of_keys - idx);
-    leaf_page->records[idx].key = key;
-    strcpy(leaf_page->records[idx].value, value);
+    int shift_sz = sizeof(LEAF(leaf_page)->records[0]);
+    memcpy(LEAF(leaf_page)->records + idx + shift_sz, LEAF(leaf_page)->records + idx, LEAF(leaf_page)->header.number_of_keys - idx);
+    LEAF(leaf_page)->records[idx].key = key;
+    strcpy(LEAF(leaf_page)->records[idx].value, value);
 
+    write_page(leaf_page);
     return 0;
 }
 
@@ -105,10 +110,11 @@ int insert_into_leaf(LeafPage * leaf_page, bpt_key_t key, c_bpt_value_t value) {
  * If success, return 0. Otherwise, return non-zero value.
  */
 int insert(bpt_key_t key, c_bpt_value_t value) {
-    LeafPage * leaf_page = find_leaf(key);
+    Page * leaf_page = find_leaf(key);
 
-    if (leaf_page->header.number_of_keys < LEAF_ORDER) {
+    if (LEAF(leaf_page)->header.number_of_keys < LEAF_ORDER) {
         insert_into_leaf(leaf_page, key, value);
+        free(leaf_page);
     }
     
     return 0;
